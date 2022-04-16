3be2b2567c6b7719faadc710cd0c82d1ffdceb80
Tung Vu <me@tungv.com>
2022-04-16 16:32:58 +0700

    refactor: add reducer and dispatch
    Signed-off-by: Tung Vu <me@tungv.com>

--- src/__tests__/useForm.tsx
+++ src/__tests__/useForm.tsx
@@ -1,41 +1,57 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import * as React from "react";
 
 import useForm from "../useForm";
 
+function MyForm() {
+  const form = useForm({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  return (
+    <form>
+      <input
+        type="text"
+        aria-label="username"
+        name="username"
+        value={form.values.username}
+        onChange={form.handleChange("username")}
+      />
+      <input
+        type="password"
+        aria-label="password"
+        value={form.values.password}
+        onChange={form.handleChange("password")}
+      />
+    </form>
+  );
+}
+
 describe("useForm", () => {
   it("should render multiple fields", () => {
-    function MyForm() {
-      const form = useForm({
-        initialValues: {
-          username: "",
-          password: "",
-        },
-      });
-
-      return (
-        <form>
-          <input
-            type="text"
-            aria-label="username"
-            name="username"
-            value={form.values.username}
-            onChange={form.handleChange("username")}
-          />
-          <input
-            type="password"
-            aria-label="password"
-            value={form.values.password}
-            onChange={form.handleChange("password")}
-          />
-        </form>
-      );
-    }
-
     const { getByLabelText } = render(<MyForm />);
 
     expect(getByLabelText("username")).toHaveValue("");
     expect(getByLabelText("password")).toHaveValue("");
+  });
+
+  it("should update values on input change", () => {
+    const { getByLabelText } = render(<MyForm />);
+
+    const usernameInput = getByLabelText("username");
+    const passwordInput = getByLabelText("password");
+
+    expect(usernameInput).toHaveValue("");
+    expect(passwordInput).toHaveValue("");
+
+    fireEvent.change(usernameInput, { target: { value: "hello" } });
+    expect(usernameInput).toHaveValue("hello");
+
+    fireEvent.change(passwordInput, { target: { value: "world" } });
+    expect(passwordInput).toHaveValue("world");
   });
 });
--- src/useForm.tsx
+++ src/useForm.tsx
@@ -1,17 +1,51 @@
+import { useReducer } from "react";
+
+type TextbaseElement =
+  | HTMLInputElement
+  | HTMLSelectElement
+  | HTMLTextAreaElement;
+
 interface FormConfig<Values> {
   initialValues: Values;
 }
 
 interface UseFormResult<Values> {
   values: Values;
-  handleChange: (field: keyof Values) => (event: React.ChangeEvent) => void;
+  handleChange: (
+    field: keyof Values,
+  ) => (event: React.ChangeEvent<TextbaseElement>) => void;
 }
+
+interface FormState<Values> {
+  values: Values;
+}
+
+type FormAction<Values> = {
+  type: "change";
+  field: keyof Values;
+  value: string;
+};
 
 export default function useForm<Values>(
   config: FormConfig<Values>,
 ): UseFormResult<Values> {
+  const [state, dispatch] = useReducer(
+    (state: FormState<Values>, action: FormAction<Values>) => {
+      return state;
+    },
+    {
+      values: config.initialValues,
+    },
+  );
+
   return {
     values: config.initialValues,
-    handleChange: () => () => {},
+    handleChange: (field) => (event) => {
+      dispatch({
+        type: "change",
+        field,
+        value: event.target.value,
+      });
+    },
   };
 }
